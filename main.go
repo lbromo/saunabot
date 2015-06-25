61babd2a1d946f5f8676f4cb9819dc4f4da848bd
Joakim Børlum <joakim@roevhat.dk>
2015-06-25 22:44:35 +0200

    Version 2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r.URL.Path[1:])
-    fmt.Fprintf(w, "SAUNABOT: %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "SAUNABOT 2.0.1: %s!", r.URL.Path[1:])
 }
 
 func main() {

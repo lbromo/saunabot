007ff5bc9d5832e419d988f52901922d7129277f
Joakim Børlum <joakim@roevhat.dk>
2015-06-29 21:36:30 +0200

    Changed token - bad

--- saunabot.js
+++ saunabot.js
@@ -3,7 +3,7 @@ var sauna    = require(__dirname + '/recipes/sauna.recipe.js');
 var kenneth  = require(__dirname + '/recipes/kenneth.recipe.js');
 var beer     = require(__dirname + '/recipes/beer.recipe.js');
 
-saunabot.init('123252775:AAGNgY8PxdgjGQ8TklY3RbEnsc3MPmnU16w', 250);
+saunabot.init('121725983:AAGJCGKpU6ni2zqLzMjlVjF2w-p5z7Q3a6U', 250);
 
 saunabot.when('/sauna', function(message) {
     sauna.run(message, function(message, reply) {

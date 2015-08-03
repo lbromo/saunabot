bf42e96c6259110d1cea0bce21b6d6d196501bc3
Joakim Børlum <joakim@roevhat.dk>
2015-08-03 18:38:00 +0200

    Sauna date

--- recipes/sauna.recipe.js
+++ recipes/sauna.recipe.js
@@ -1,8 +1,14 @@
 (function() {
     var run = function(message, callback) {
-        if (typeof callback === 'function')
-            callback(message, 'Det ved jeg fandme ikke!');
-    };
+        if (typeof callback === 'function') {
+            var saunaDate = new Date("08/04/2015");
+            var todaysDate = new Date();
 
+            if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+                callback(message, 'Ja forhelvede! Vi skal i sauna!');
+            }
+            callback(message, 'Røvhul! Ingen sauna i dag!!!');
+        }
+    };
     module.exports.run = run;
 }());

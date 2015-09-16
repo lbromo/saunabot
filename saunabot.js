0bec4f54dd0e0274e9c2eb3492cbd8e3df9ac767
Mike Røntved <mikeront16@gmail.com>
2015-09-16 11:22:35 +0200

    Burger added

--- saunabot.js
+++ saunabot.js
@@ -3,6 +3,7 @@ var sauna    = require(__dirname + '/recipes/sauna.recipe.js');
 var kenneth  = require(__dirname + '/recipes/kenneth.recipe.js');
 var simon    = require(__dirname + '/recipes/simon.recipe.js');
 var beer     = require(__dirname + '/recipes/beer.recipe.js');
+var burger	 = require(__dirname + '/recipes/burger.recipes.js');
 
 saunabot.init('121725983:AAGJCGKpU6ni2zqLzMjlVjF2w-p5z7Q3a6U', 250);
 
@@ -20,4 +21,12 @@ saunabot.when('/simon', function(message) {
 
 saunabot.when('/bajer', function(message) {
     beer.run(message, saunabot.reply);
+});
+
+saunabot.when('/burger', function(message) {
+	burger.run(message, saunabot.reply);
+});
+
+saunabot.when('/:hamburger:', function(message) {
+	burger.run(message, saunabot.reply);
 });
